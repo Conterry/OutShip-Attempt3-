abdafb67ab0039f5348b08d7e32daf821015e2da
Conterry <59209260+Conterry@users.noreply.github.com>
2020-08-07 19:36:54 +0300

    WIP

---
+++ Assets/StartSeamouve.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSeamouve : MonoBehaviour
+{
+    
+    public GameObject Sea;
+    public int GameSpeed;
+
+    void Update()
+    {
+        Sea.transform.Translate(Vector3.back * Time.deltaTime * GameSpeed);
+    }
+
+}
---
+++ Assets/trigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("gggg");
+    }
+}
